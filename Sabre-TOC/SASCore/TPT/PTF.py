f68a48852241975d97c4b8c4efd88dcb43d3eb20
aidden laoch <aidden.laoch@gmail.com>
2021-01-15 15:06:50 -0700

    convert intention to be consistant main.py

--- Sabre-TOC/SASCore/TPT/PTF.py
+++ Sabre-TOC/SASCore/TPT/PTF.py
@@ -6,48 +6,48 @@ from .settings import r
 class ptf():
 
 	def __init__(self, teamserver, HOST, PORT):
-                self.ts = teamserver
-                self.h = HOST
-                self.p = PORT
-                self.lid = "%s:%s" % (HOST,PORT)
-                self.channel = self.lid + '-cmd'
-                self.pubsub = self.ts.pubsub()
-                self.ts.lpush(self.lid, 'hostneame')
-                self.result = self.lid + '-out'
-                self.ts.publish('Listeners', self.lid)
+		self.ts = teamserver
+		self.h = HOST
+		self.p = PORT
+		self.lid = "%s:%s" % (HOST,PORT)
+		self.channel = self.lid + '-cmd'
+		self.pubsub = self.ts.pubsub()
+		self.ts.lpush(self.lid, 'hostneame')
+		self.result = self.lid + '-out'
+		self.ts.publish('Listeners', self.lid)
 
-        def screenPTF(self):
-                print('Starting the "PenTest Framework" in session')
-                try:
-                        l = subprocess.check_output(['tmux', 'list-sessions'], shell=False)
-                        li = l.split('\n')
-                        n = []
-                        for i in li:
-                                if 'ptf' in i:
-                                        n.append(i.split('ptf')[1])
-                        c = 0
-                        nu = []
-                        for i in n:
-                                if ':' not in i[0]:
-                                        nu.append(i[0])
-                                else:
-                                        nu.append(str(0))
-                        if nu:
-                                nn = str(int(max(nu)) + 1)
-                except:
-                        l = 'empty' #No sessions
-                # high number 
-                if 'ptf' in l:
-                        print('''
+	def screenPTF(self):
+		print('Starting the "PenTest Framework" in session')
+		try:
+			l = subprocess.check_output(['tmux', 'list-sessions'], shell=False)
+			li = l.split('\n')
+			n = []
+			for i in li:
+				if 'ptf' in i:
+					n.append(i.split('ptf')[1])
+			c = 0
+			nu = []
+			for i in n:
+				if ':' not in i[0]:
+					nu.append(i[0])
+				else:
+					nu.append(str(0))
+			if nu:
+				nn = str(int(max(nu)) + 1)
+		except:
+			l = 'empty' #No sessions
+		# high number 
+		if 'ptf' in l:
+			print('''
 ERROR! Already have one!:
 ''')
-                        subprocess.call(["/bin/bash", "-c", "tmux new -s ptf%d 'clear && cd /root/ptf && ./ptf'" % int(nn)])
-                        print('ptf%d' % int(nn))
-                else:
-                        subprocess.call(["/bin/bash", "-c", "tmux new -s ptf 'clear && cd /root/ptf && ./ptf'"])
+			subprocess.call(["/bin/bash", "-c", "tmux new -s ptf%d 'clear && cd /root/ptf && ./ptf'" % int(nn)])
+			print('ptf%d' % int(nn))
+		else:
+			subprocess.call(["/bin/bash", "-c", "tmux new -s ptf 'clear && cd /root/ptf && ./ptf'"])
 
 
 if __name__ == "__main__":
-        #r = redis.Redis(host='localhost', port=6379)
-        client = ptf(r, "0.0.0.0", 42)
-        client.screenPTF()
+	#r = redis.Redis(host='localhost', port=6379)
+	client = ptf(r, "0.0.0.0", 42)
+	client.screenPTF()
--- Sabre-TOC/SASCore/TPT/Shodan.py
+++ Sabre-TOC/SASCore/TPT/Shodan.py
@@ -7,7 +7,7 @@ import time
 class shodan():
 
 	def __init__(self, teamserver, HOST, PORT):
-        	self.ts = teamserver
+		self.ts = teamserver
 		self.h = HOST
 		self.p = PORT
 		self.lid = "%s:%s" % (HOST,PORT)
@@ -17,35 +17,35 @@ class shodan():
 		self.result = self.lid + '-out'
 		self.ts.publish('Listeners', self.lid)
 
-        def screenShodan(self):
-                print('Starting shodan CLI in session')
-                try:
-                        l = subprocess.check_output(['tmux', 'list-sessions'], shell=False)
-                        li = l.split('\n')
-                        n = []
-                        for i in li:
-                                if 'shodan' in i:
-                                        n.append(i.split('shodan')[1])
-                        c = 0
-                        nu = []
-                        for i in n:
-                                if ':' not in i[0]:
-                                        nu.append(i[0])
-                                else:
-                                        nu.append(str(0))
-                        if nu:
-                                nn = str(int(max(nu)) + 1)
-                except:
-                        l = 'empty' #No sessions
+	def screenShodan(self):
+		print('Starting shodan CLI in session')
+		try:
+			l = subprocess.check_output(['tmux', 'list-sessions'], shell=False)
+			li = l.split('\n')
+			n = []
+			for i in li:
+				if 'shodan' in i:
+					n.append(i.split('shodan')[1])
+			c = 0
+			nu = []
+			for i in n:
+				if ':' not in i[0]:
+					nu.append(i[0])
+				else:
+					nu.append(str(0))
+			if nu:
+				nn = str(int(max(nu)) + 1)
+		except:
+			l = 'empty' #No sessions
 		# high number
 		print('Attempting to start sessions for shodan!')
 		time.sleep(2)
 		if self.h in l:
 			print('already have one')
-                        subprocess.call(['/bin/bash', '-c', 'tmux new -s shodan%d "shodan -h && /bin/bash"' % int(nn)])
+			subprocess.call(['/bin/bash', '-c', 'tmux new -s shodan%d "shodan -h && /bin/bash"' % int(nn)])
 			print('msfconsole%d' % int(nn))
 		else:
-                        subprocess.call(["/bin/bash", "-c", "tmux new -s shodan 'shodan -h && /bin/bash'" ])
+			subprocess.call(["/bin/bash", "-c", "tmux new -s shodan 'shodan -h && /bin/bash'" ])
 
 if __name__ == "__main__":
 	#r = redis.Redis(host='localhost', port=6379)
