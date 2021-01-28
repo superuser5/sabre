cd29456076b02b771d39222e543d42456549ac84
aidden <aidden@kali.revlab.local>
2021-01-28 16:20:48 -0700

    Updated installer to reflect new interface

--- Sabre-Cli/install.sh
+++ Sabre-Cli/install.sh
@@ -8,6 +8,10 @@ sudo /bin/cp -R ./* /opt/Sabre-Cli/
 sudo ln -s /opt/Sabre-Cli/Sabre-Web/Sabre-Cli/pssh.py /usr/bin/sabre-cli
 sudo ln -s /opt/Sabre-Cli/Sabre-Web/sabre-web.sh /usr/bin/sabre-web
 
+cd /opt/Sabre-Cli/Sabre-Web/
+python3 -m venv server
+/opt/Sabre-Cli/Sabre-Web/server/bin/pip install -r ./requirements.txt
+
 echo '''
 
 ###############################################################
@@ -17,6 +21,8 @@ echo '''
 #                     #####################                   #
 #                                                             #
 # Run "sabre-cli -u {Sabre User} -s {Sabre-TOC IP or Domain}" #
+# Run "sabre-web {Give a username and server to connect to}"  #
+#	- Then connect to http://127.0.0.1:5000		      #
 #                                                             #
 ###############################################################
 
