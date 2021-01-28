823c44a6394c301fa029becce7fd7fcb7699833d
aidden <aidden@kali.revlab.local>
2021-01-28 16:07:33 -0700

    Added script to start web interface

---
+++ Sabre-Cli/Sabre-Web/sabre-web.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+#cd /opt/Sabre-Cli/Sabre-Web/
+
+read -p 'Username: ' user
+read -p 'TOC Server: ' server
+
+server/bin/python -m sabre-web --command sabre-cli --cmd-args "-u $user -s $server"
--- Sabre-Cli/install.sh
+++ Sabre-Cli/install.sh
@@ -5,7 +5,8 @@ pip install pexpect
 sd=`pwd`
 sudo mkdir /opt/Sabre-Cli
 sudo /bin/cp -R ./* /opt/Sabre-Cli/
-sudo ln -s /opt/Sabre-Cli/pssh.py /usr/bin/sabre-cli
+sudo ln -s /opt/Sabre-Cli/Sabre-Web/Sabre-Cli/pssh.py /usr/bin/sabre-cli
+sudo ln -s /opt/Sabre-Cli/Sabre-Web/sabre-web.sh /usr/bin/sabre-web
 
 echo '''
 
