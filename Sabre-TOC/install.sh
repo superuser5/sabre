baaecf6b37e6053e125d600639b36f2151f1e13e
aidden laoch <aidden.laoch@gmail.ciom>
2022-05-31 15:39:03 -0400

    updated to add basic nim implant

--- Sabre-TOC/install.sh
+++ Sabre-TOC/install.sh
@@ -22,7 +22,7 @@ mkdir -p /home/root/.sabre/
 
 apt update -y
 sleep 2
-apt install -y redis-server python-redis openssh-server nmap git python-pip3 openssh-server screen tmux moreutils python3-venv -y #oracle-java8-installer if we add MSF back to the main installer
+apt install -y redis-server nim python-redis openssh-server nmap git python-pip3 openssh-server screen tmux moreutils python3-venv ming-x64 #oracle-java8-installer if we add MSF back to the main installer
 sleep 2
 apt upgrade
 sleep 2
@@ -44,19 +44,6 @@ bind-key C-a send-prefix
 ''' >> /root/.tmux.conf
 
 echo '''
-* need to make this an option
-#############################
-# Installing TrustedSec PTF # 
-#############################
-
-'''
-
-#cd /root/
-#git clone https://github.com/trustedsec/ptf.git
-#cd $sd
-
-
-echo '''
 
 
 ###############################################
@@ -68,41 +55,36 @@ echo '''
 #sed -i 's/PermitRootLogin\ prohibit-password/PermitRootLogin\ without-password/' /etc/ssh/sshd_config #commented this out for docker version
 #service ssh restart
 
-#debug
-#sleep 3
+echo '''
+
+
+###############################################
+# Configuring other needed settings for Sabre #
+###############################################
+
+'''
+
+nimble -y install strenc
+nimble -y install winim
+nimble -y install zippy
+nimble -y install nimcrypto
 
 pip3 install --upgrade pip3
 
 pip3 install -r pip3-install-me.txt
-#pip3 install covertutils
-#pip3 install cmd2==0.7.9
-#pip3 install pexpect
-#pip3 install scapy
 
-# need to make this optional
-#snap install rocketchat-server
-
-#Need to fix these SED commands
 ln -s $sd/SASCore/Listeners/simple.py /usr/bin/simple.py
 ln -s $sd/s-cli.py /usr/bin/sabre
 ln -s $sd/update.sh /usr/bin/sabre-update
+#Need to fix the SED commands
 #sed -i “s/bind\ 127.0.0.1/bind\ 127.0.0.1\ $ip/“ /etc/redis/redis.conf
-
-#debug
-#sleep 3
 
 #Need to fix this one as well
 #sed -i “s/localhost/$ip/“ $d/SASCore/Listeners/settings.py
-
-#debug
-#sleep 3
-
-
-#sed -i 's/localhost/$ip/' $d/SASCore//settings.py
+#sed -i 's/localhost/$ip/' $d/SASCore/settings.py
 
 systemctl enable redis
 redis-server &
-#cp /usr/bin/pip3 /usr/local/bin/pip3 #there is an issue with pip3 after install. something reaks pip3 in /usr/local/bin but the /usr/bin/ version works so I am replacing the local/bin one for now
 sleep 5
 echo '''
 
